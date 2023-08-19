c854fec3bd0afccfe52e28dccd17037ecff4197c
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-08-19 11:25:42 +0300

    Add example for serde_derive

---
+++ examples/crate_universe_failure/serde_derive/src/main.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    let _point = Point { x: 1, y: 2 };
+}
